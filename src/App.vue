9b87417dece9f96fcfad9bb761d61e79b323e34c
PavelMalashenko <82096789+PvlMl@users.noreply.github.com>
2022-02-08 00:20:29 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div style="margin-top: 50px">
+      <line-chart
+        label="Positive test results"
+        :labels="reversedArrDates"
+        :chart-data="reversedDatasetsPositive"
+        :options="options"
+        :color="colorToPositive"
+      />
+    </div>
+    <div style="margin-top: 50px">
+      <line-chart
+        label="Negative test results"
+        :labels="reversedArrDates"
+        :chart-data="reversedDatasetsNegative"
+        :options="options"
+        :color="colorToNegative"
+      />
+    </div>
+    <div style="margin-top: 50px">
+      <line-chart
+        label="Death"
+        :labels="reversedArrDates"
+        :chart-data="reversedDatasetsDeath"
+        :options="options"
+        :color="colorToDeath"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import LineChart from "./components/LineChart";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    LineChart,
+  },
+  data() {
+    return {
+      arrDates: [],
+      datasetsPositive: [],
+      colorToPositive: "#f87979",
+      datasetsNegative: [],
+      colorToNegative: "#d3e298",
+      datasetsDeath: [],
+      colorToDeath: "#353531",
+      options: { responsive: true, maintainAspectRatio: false },
+    };
+  },
+  methods: {
+    formatDate(data) {
+      let formatDate = data.toString().split("");
+      formatDate.splice(4, 0, "-");
+      formatDate.splice(7, 0, "-");
+      return formatDate.join("");
+    },
+  },
+  computed: {
+    reversedArrDates() {
+      return this.arrDates.slice().reverse();
+    },
+    reversedDatasetsPositive() {
+      return this.datasetsPositive.slice().reverse();
+    },
+    reversedDatasetsNegative() {
+      return this.datasetsNegative.slice().reverse();
+    },
+    reversedDatasetsDeath() {
+      return this.datasetsDeath.slice().reverse();
+    },
+  },
+  mounted() {
+    axios.get("https://api.covidtracking.com/v1/us/daily.json").then((r) =>
+      r.data.forEach((el) => {
+        this.arrDates.push(this.formatDate(el.date));
+        this.datasetsPositive.push(el.positive);
+        this.datasetsNegative.push(el.negative);
+        this.datasetsDeath.push(el.death);
+      })
+    );
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,40 @@
+
+<script>
+import { Line } from "vue-chartjs";
+
+export default {
+  extends: Line,
+  props: {
+    label: {
+      type: String,
+    },
+    labels: {
+      type: Array,
+    },
+    chartData: {
+      type: Array,
+    },
+    options: {
+      type: Object,
+    },
+    color: {
+      type: String,
+    },
+  },
+  mounted() {
+    this.renderChart(
+      {
+        labels: this.labels,
+        datasets: [
+          {
+            label: this.label,
+            data: this.chartData,
+            backgroundColor: this.color,
+          },
+        ],
+      },
+      this.options
+    );
+  },
+};
+</script>
